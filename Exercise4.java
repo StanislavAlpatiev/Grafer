22fa5ec5dc69c885f1bacb2d475aa7e23e80d048
StanislavAlpatiev <60314985+StanislavAlpatiev@users.noreply.github.com>
2021-04-16 14:09:16 +0200

    Add files via upload

---
+++ Exercise4.java
@@ -0,0 +1,16 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Exercise4 implements Ex4 {
+	private final Graph<Nodes.GraphNode> graph;
+
+	public Exercise4(Graph<Nodes.GraphNode> graph) {
+		this.graph = graph;
+	}
+
+	
+}
